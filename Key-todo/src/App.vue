273a9f3b68c4681b3bd6f59a1b8b88e6f86bb952
hmx <1037107465@qq.com>
2024-04-25 22:55:22 +0800

    memorize

---
+++ Key-todo/src/App.vue
@@ -0,0 +1,88 @@
+<script>
+export default {
+  data () {
+    return {
+      todos:[
+        {
+          id:123123,
+          title:'test content1',
+          completed: false
+        },
+        {
+          id:103710,
+          title:'test content2',
+          completed:true
+        },
+      ]
+    }
+  },
+  methods:{
+    toggleAll(e){
+      this.todos.forEach(todo=>todo.completed=e.target.checked)
+    },
+    onHashChange(){
+      
+    }
+  },
+  mounted (){
+    window.addEventListener('hashchange',this.onHashChange)
+  }
+  
+}
+      
+
+</script>
+
+<template>
+  <section class="todoapp">
+			<header class="header">
+				<h1>todos</h1>
+				<input class="new-todo" placeholder="What needs to be done?" autofocus>
+			</header>
+			<!-- This section should be hidden by default and shown when there are todos -->
+			<section class="main">
+				<input id="toggle-all" class="toggle-all" type="checkbox" @click="toggleAll">
+				<label for="toggle-all">Mark all as complete</label>
+				<ul class="todo-list">
+					<!-- These are here just to show the structure of the list items -->
+					<!-- List items should get the class `editing` when editing and `completed` when marked as completed -->
+					<li
+          v-for = "todo in todos"
+          :key="todo.id"
+          :class="{completed:todo.completed}">
+						<div class="view">
+							<input class="toggle" type="checkbox" v-model="todo.completed">
+              <!-- v-model双向绑定，输入框的值影响页面渲染 ：class这是单向绑定 -->
+              <!-- 这个第一个注释错误了，其实这是个checkbox的双向绑定，不是上面的文本框 -->
+							<label v-text="todo.title"></label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="Create a TodoMVC template">
+					</li>
+				</ul>
+			</section>
+			<!-- This footer should be hidden by default and shown when there are todos -->
+			<footer class="footer">
+				<!-- This should be `0 items left` by default -->
+				<span class="todo-count"><strong>0</strong> item left</span>
+				<!-- Remove this if you don't implement routing -->
+				<ul class="filters">
+					<li>
+						<a href="#/">All</a>
+					</li>
+					<li>
+						<a href="#/active">Active</a>
+					</li>
+					<li>
+						<a href="#/completed">Completed</a>
+					</li>
+				</ul>
+				<!-- Hidden if no completed items are left ↓ -->
+				<button class="clear-completed">Clear completed</button>
+			</footer>
+		</section>
+</template>
+
+<style >
+@import "https://unpkg.com/todomvc-app-css@2.4.1/index.css";
+</style>
